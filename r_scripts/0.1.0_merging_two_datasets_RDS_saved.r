90a2913230a3a7ab2c13f336a286946212e5c302
Arsham Mikaeili Namini <92790984+Arshammik@users.noreply.github.com>
2023-12-29 03:21:09 -0500

    making mtx1_logsclaed

--- r_scripts/0.1.0_merging_two_datasets_RDS_saved.r
+++ r_scripts/0.1.0_merging_two_datasets_RDS_saved.r
@@ -23,24 +23,31 @@ for(i in 1:length(file_paths)){
 mtx1 <- cbind(TD1, TD2, TD3, TD4, TD5, TD6, TD7, TD8, TD9)
 dim(mtx1)
 
-#QC
-colsums_mtx1 <- hist(colSums(mtx1))
-
-
-mtx2 <- readRDS("~/Documents/single_cell/nsclc_project/data/GSE131907_Lung_Cancer_normalized_log2TPM_matrix_filtered.rds")
-mtx2 <- bigmemory::as.matrix(mtx2)
-mtx2 <- Matrix(mtx2, sparse = TRUE)
+mtx2 <- readRDS("~/Documents/single_cell/nsclc_project/data/GSE131907_sparse_matrix.rds")
 dim(mtx2)
+
+
+#QC
+colsums_mtx1 <- Matrix::colSums(mtx1)
+colsums_mtx2 <- Matrix::colSums((mtx2))
+
+mtx1_log2 <- Matrix::Matrix(log2(mtx1 + 1), sparse = TRUE)
+colsums_mtx1_log2 <- Matrix::colSums(mtx1_log2)
+
+hist(colsums_mtx1, main = "Original mtx1")
+hist(log2(colSums(mtx1) + 1), main = "Log2 Scaled colsums(mtx1)")
+hist(colsums_mtx2, main = "Original mtx2")
+hist(colsums_mtx1_log2, main = "Log2 Scaled mtx1")
+hist(log2(colsums_mtx1_log2 + 1))
 
 
 features_mtx1 <- rownames(mtx1)
 features_mtx2 <- rownames(mtx2)
 features <- intersect(features_mtx1, features_mtx2)
 
-mtx1 <- mtx1[features,]
+mtx1_log2 <- mtx1_log2[features,]
 mtx2 <- mtx2[features,]
 dim(mtx1) ; dim(mtx2)
 
-mtx <- cbind(mtx1, mtx2)
+mtx <- cbind(mtx1_log2, mtx2)
 saveRDS(mtx, '~/Documents/Single Cell/Own Project/Data/mtx(GSE189357 & GSE131907).rds')
-
