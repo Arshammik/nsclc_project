c248674e8612ec69e830a93984e858eaae7a2d6a
Arsham Mikaeili Namini <92790984+Arshammik@users.noreply.github.com>
2023-12-31 18:38:51 -0500

    fixing bugs

--- r_scripts/0.1.1_assessing_the_batch_effect_and_performing_seurat_pipeline.r
+++ r_scripts/0.1.1_assessing_the_batch_effect_and_performing_seurat_pipeline.r
@@ -2,6 +2,7 @@ library(Seurat)
 library(SeuratData)
 library(patchwork)
 library(dplyr)
+library(tidyverse)
 library(ggplot2)
 library(GEOquery)
 
@@ -9,14 +10,12 @@ mtx <- readRDS("~/Documents/single_cell/nsclc_project/data/mtx.rds")
 pbmc <- CreateSeuratObject(counts = mtx, project = 'nsclc', min.cells = 2, min.features = 200)
 pbmc[["mt.percent"]] <- PercentageFeatureSet(object = pbmc, pattern = "^MT-")
 
-
 VlnPlot(object = pbmc, features = c("nFeature_RNA", "nCount_RNA", "mt.percent"), ncol = 3, raster = FALSE)
 pbmc <- subset(pbmc, subset = nFeature_RNA < 5000 & nCount_RNA < 10000 & mt.percent < 5)
+rownames_meat <- rownames(pbmc@meta.data)
+pbmc@meta.data$barcode <- rownames_meat
+pbmc@meta.data$title <- sub("(^.{17})(.*$)","\\2",rownames_meat)
 
-metadata_rownames <- rownames(pbmc@meta.data)
-metadata_rownames <- sub("(^.*?_)(.*$)", "\\2", metadata_rownames)
-pbmc@meta.data$ID<- metadata_rownames 
-pbmc@meta.data$title<- metadata_rownames 
 
 # we have two GEO data sets 
 uniqie_rownames <- unique(metadata_rownames)
@@ -28,7 +27,6 @@ colnames(GSE131907_pheno) <- c("title", "geo_accession", "tissue_source", "tumor
 GSE131907_pheno <- GSE131907_pheno[GSE131907_pheno$title %in% uniqie_rownames,]
 GSE131907_pheno[1:11,6] <- "N"
 
-
 #GSE189357
 GSE189357 <- getGEO(GEO = "GSE189357", GSEMatrix = TRUE)
 GSE189357_pheno <- pData(GSE189357[[1]])
@@ -36,11 +34,11 @@ GSE189357_pheno <- GSE189357_pheno[,c(1,2,10,11,12)]
 GSE189357_pheno$title <- sub(" scRNA-seq","",GSE189357_pheno$title)
 GSE189357_pheno$stage <- c("IV", "IV", "I", "I", "0", "I", "0", "0", "IV")
 
-
 sample_stages <- rbind(GSE131907_pheno[,c(1,6)], GSE189357_pheno[,c(1,6)])
+sample_stages[37:45,1] <- paste0("1_", sample_stages[37:45,1])
+
 pbmc@meta.data <- merge(pbmc@meta.data, sample_stages, by = "title")
-#pbmc@meta.data <- pbmc@meta.data[order(pbmc@meta.data$stage),]
-table(pbmc@meta.data$stage)
+rownames(pbmc@meta.data) <- pbmc@meta.data$barcode
 
 pbmc <- NormalizeData(object = pbmc, normalization.method = "RC", scale.factor = 1e6)
 pbmc <- FindVariableFeatures(object = pbmc, selection.method = "vst", loess.span = 0.3, mean.function = 2000)
@@ -51,7 +49,5 @@ pbmc <- FindNeighbors(object = pbmc, dims = 1:20)
 pbmc <- FindClusters(object = pbmc, resolution = 0.5)
 pbmc <- RunUMAP(object = pbmc, dims = 1:20)
 
-
-DimPlot(object = pbmc, reduction = "umap", raster = FALSE, group.by = "stage")
-
+DimPlot(object = pbmc, reduction = "umap", group.by = "stage", raster = FALSE)
 
