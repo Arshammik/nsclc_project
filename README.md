c52dcd2d10578355e5d7f860f72f55dcdd1dfca5
Arsham Mikaeili Namini <92790984+Arshammik@users.noreply.github.com>
2024-01-07 01:52:02 -0500

    Initiating the README file

---
+++ README.md
@@ -0,0 +1,3 @@
+####__Batch effect removel__
+
+Since we integrated two datasets we should worry about the bathc eff3ct migth affects
--- r_scripts/0.1.3_assessing_batch_removal.r
+++ r_scripts/0.1.3_assessing_batch_removal.r
@@ -8,4 +8,21 @@ suppressPackageStartupMessages(library(GEOquery))
 suppressPackageStartupMessages(library(BatchQC))
 
 merged_Seurat_object <- "~/Documents/single_cell/nsclc_project/results/seurat.integrated.rds"
+pbmc_before_batch_effect_removal <- "~/Documents/single_cell/nsclc_project/results/pbmc(before_batch_effect_removal).rds"
 merged_pbmc <- readRDS(merged_Seurat_object)
+pbmc <- readRDS(pbmc_before_batch_effect_removal)
+
+
+#scaling and dimensional reductions
+merged_pbmc <- ScaleData(object = merged_pbmc)
+merged_pbmc <- RunPCA(object = merged_pbmc)
+merged_pbmc <- RunUMAP(object = merged_pbmc, dims = 1:50)
+
+png("../results/plots/1.1_DimPlot_pbmc.png", width = 1000, height = 1000)
+DimPlot(object = pbmc, reduction = "umap",group.by = "stage", raster = FALSE)
+dev.off()
+
+png("../results/plots/1.2_DimPlot_mergedpbmc.png", width = 1000, height = 1000)
+DimPlot(object = merged_pbmc, reduction = "umap",group.by = "stage", raster = FALSE)
+dev.off()
+
